81f63b0c0f409262bd5c02ee46736eebf7a9edf7
LudwigEth <ludwig.eth@gmail.com>
2023-09-21 19:56:38 +0200

    Feat: add createBookCard(title, author, pages)
    this function creates a new book card and adds it to the library
    
    it does following:
    
    1. check input types
    2. create container div and content elements
    3. adds a class to each element
    4. sets attribute of button to the button element
    5. sets textcontent of content elements to user input
    6. appends all elements to the container div and then to the library

--- script.js
+++ script.js
@@ -21,3 +21,33 @@ function addBookToLibrary(title, author, pages) {
 
     library.push(newBook);
 };
+
+const createBookCard = (title, author, pages) => {
+    if (hasInvalidType(title, author, pages)) return;
+
+    const newBookDiv = document.createElement('div');
+    const bookTitleElement = document.createElement('h3');
+    const bookAuthorElement = document.createElement('address');
+    const bookPagesElement = document.createElement('p');
+    const readOrNotButton = document.createElement('button')
+
+    newBookDiv.classList.add('new-book');
+    bookTitleElement.classList.add('book-title');
+    bookAuthorElement.classList.add('book-author');
+    bookPagesElement.classList.add('book-pages');
+    readOrNotButton.classList.add('not-read');
+
+    readOrNotButton.setAttribute('type', 'button');
+
+    bookTitleElement.textContent = title;
+    bookAuthorElement.textContent = author;
+    bookPagesElement.textContent = pages;
+    readOrNotButton.textContent = 'not read';
+
+    newBookDiv.appendChild(bookTitleElement);
+    newBookDiv.appendChild(bookAuthorElement);
+    newBookDiv.appendChild(bookPagesElement);
+    newBookDiv.appendChild(readOrNotButton);
+
+    libraryGrid.appendChild(newBookDiv);
+};
